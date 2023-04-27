35c995bf14e14c8d1af44ebc0a0bd5ae1bdb830a
shailender singh <shailender_bakshi@yahoo.com>
2023-04-27 18:24:35 +0000

    Updated main.tf

--- AKS/cluster/main.tf
+++ AKS/cluster/main.tf
@@ -5,8 +5,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "First-aks"
-  location            = azurerm_resource_group.var.location
-  resource_group_name = azurerm_resource_group.var.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "First-k8s"
 
   default_node_pool {
@@ -16,10 +16,10 @@ resource "azurerm_kubernetes_cluster" "default" {
     os_disk_size_gb = 30
   }
 
-  service_principal {
-    client_id     = var.appId
-    client_secret = var.password
-  }
+//   service_principal {
+//     client_id     = var.appId
+//     client_secret = var.password
+//   }
 
   role_based_access_control_enabled = true
 

f450dc6aba492a8d97dd8b9fbd062adba055c78d
shailender singh <shailender_bakshi@yahoo.com>
2023-04-29 08:19:15 +0000

    Updated vnet.tf

--- azuredeploy/modules/vnet/vnet.tf
+++ azuredeploy/modules/vnet/vnet.tf
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "virtual_network" {
   name = var.name
   location = var.location
   resource_group_name = azurerm_resource_group.vnet_resource_group.name
-  address_space = [var.network_address_space]
+  address_space = var.network_address_space
 
   tags = {
     Environment = var.environment
@@ -23,12 +23,12 @@ resource "azurerm_subnet" "aks_subnet" {
   name = var.aks_subnet_address_name
   resource_group_name  = azurerm_resource_group.vnet_resource_group.name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes = [var.aks_subnet_address_prefix]
+  address_prefixes = var.aks_subnet_address_prefix
 }
 
 resource "azurerm_subnet" "appgw_subnet" {
   name = var.appgw_subnet_address_name
   resource_group_name  = azurerm_resource_group.vnet_resource_group.name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes = [var.appgw_subnet_address_prefix]
+  address_prefixes = var.appgw_subnet_address_prefix
 }

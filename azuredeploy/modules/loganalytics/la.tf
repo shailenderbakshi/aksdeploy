3a9bbacc8353f9731ebf285b4346689ad00807c5
shailender singh <shailender_bakshi@yahoo.com>
2023-05-05 10:12:10 +0000

    Updated la.tf

--- azuredeploy/modules/loganalytics/la.tf
+++ azuredeploy/modules/loganalytics/la.tf
@@ -1,16 +1,16 @@
-resource "azurerm_resource_group" "kubernetes_resource_group" {
-  location = var.location
-  name     = "${var.log_analytics_workspace_name}-rg"
-  tags = {
-    Environment = var.environment
-  }
-}
+// resource "azurerm_resource_group" "kubernetes_resource_group" {
+//   location = var.location
+//   name     = "${var.log_analytics_workspace_name}-rg"
+//   tags = {
+//     Environment = var.environment
+//   }
+// }
 
 resource "azurerm_log_analytics_workspace" "Log_Analytics_WorkSpace" {
     # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
     name                = var.log_analytics_workspace_name
+    resource_group_name = var.resource_group_name
     location            = var.location
-    resource_group_name = azurerm_resource_group.kubernetes_resource_group.name
     sku                 = var.log_analytics_workspace_sku
       tags = {
     Environment = var.environment

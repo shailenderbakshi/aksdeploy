768bc944b06ce792c2361ca7572598bd694feef8
shailender singh <shailender_bakshi@yahoo.com>
2023-05-05 14:01:05 +0000

    Updated la.tf

--- azuredeploy/modules/loganalytics/la.tf
+++ azuredeploy/modules/loganalytics/la.tf
@@ -20,7 +20,7 @@ resource "azurerm_log_analytics_workspace" "Log_Analytics_WorkSpace" {
 resource "azurerm_log_analytics_solution" "Log_Analytics_Solution_ContainerInsights" {
     solution_name         = "ContainerInsights"
     location              = azurerm_log_analytics_workspace.Log_Analytics_WorkSpace.location
-    resource_group_name   = azurerm_resource_group.kubernetes_resource_group.name
+    resource_group_name   = azurerm_resource_group.rg.name
     workspace_resource_id = azurerm_log_analytics_workspace.Log_Analytics_WorkSpace.id
     workspace_name        = azurerm_log_analytics_workspace.Log_Analytics_WorkSpace.name
 

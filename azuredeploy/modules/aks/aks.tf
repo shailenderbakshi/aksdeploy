cf0bf15c5df141efd654abd48324301d9fc05e54
shailender singh <shailender_bakshi@yahoo.com>
2023-05-05 16:34:27 +0000

    Updated aks.tf

--- azuredeploy/modules/aks/aks.tf
+++ azuredeploy/modules/aks/aks.tf
@@ -8,8 +8,8 @@
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   name                = var.name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
   dns_prefix          = var.name
 
   // node_resource_group = var.resource_group_name

f6f1e905cb0d9747aec95799076dbe1e20861ca2
shailender singh <shailender_bakshi@yahoo.com>
2023-05-05 09:03:10 +0000

    Updated aks.tf

--- azuredeploy/modules/aks/aks.tf
+++ azuredeploy/modules/aks/aks.tf
@@ -1,6 +1,6 @@
-resource "azurerm_resource_group" "kubernetes_resource_group" {
+resource "azurerm_resource_group" "rg" {
   location = var.location
-  name     = "${var.name}-rg"
+  name     = var.resource_group_name
   tags = {
     Environment = var.environment
   }

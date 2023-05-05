57688d7038303a98715f08b761dbe3d0636b159e
shailender singh <shailender_bakshi@yahoo.com>
2023-05-05 14:56:36 +0000

    Updated outputs.tf

--- azuredeploy/modules/vnet/outputs.tf
+++ azuredeploy/modules/vnet/outputs.tf
@@ -14,9 +14,9 @@ output "vnet_name" {
   value = azurerm_virtual_network.virtual_network.name
 }
 
-output "resource_group" {
-  value = azurerm_resource_group.vnet_resource_group.name
-}
+// output "resource_group" {
+//   value = azurerm_resource_group.vnet_resource_group.name
+// }
 
 output "resource_group_id" {
   value = azurerm_resource_group.vnet_resource_group.id

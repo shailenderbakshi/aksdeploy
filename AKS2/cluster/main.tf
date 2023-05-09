ec6f04d6c321cbc4d24dc939c3c03a939e1e6d9c
shailender singh <shailender_bakshi@yahoo.com>
2023-05-09 09:03:46 +0000

    Updated main.tf

--- AKS2/cluster/main.tf
+++ AKS2/cluster/main.tf
@@ -1,29 +1,49 @@
+#main.tf
+
+data "http" "aksc_release" {
+  url = "https://github.com/Azure/AKS-Construction/releases/download/0.9.14/main.json"
+  request_headers = {
+    Accept = "application/json"
+    User-Agent = "request module"
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
+  name = var.resourceGroupName
   location = var.location
-  name     = var.resource_group_name
 }
 
-resource "azurerm_kubernetes_cluster" "default" {
-  name                = "First-aks"
-  location            = azurerm_resource_group.rg.location
+resource "azurerm_resource_group_template_deployment" "aksc_deploy" {
+  name = "AKS-C"
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "First-k8s"
-
-  default_node_pool {
-    name            = "default"
-    node_count      = 2
-    vm_size         = "Standard_B2s"
-    os_disk_size_gb = 30
-  }
-
-  service_principal {
-    client_id     = var.appId
-    client_secret = var.password
-  }
-
-  role_based_access_control_enabled = true
-
-  tags = {
-    environment = "Demo"
-  }
-}
+  deployment_mode = "Incremental"
+  template_content = data.http.aksc_release.response_body
+  parameters_content = jsonencode({
+    resourceName = {value=var.resourceName}
+    upgradeChannel = {value=var.upgradeChannel}
+    AksPaidSkuForSLA = {value=var.AksPaidSkuForSLA}
+    agentCountMax = {value=var.agentCountMax}
+    custom_vnet = {value=var.custom_vnet}
+    enable_aad = {value=var.enable_aad}
+    AksDisableLocalAccounts = {value=var.AksDisableLocalAccounts}
+    enableAzureRBAC = {value=var.enableAzureRBAC}
+    adminPrincipalId = {value=data.azurerm_client_config.current.object_id}
+    registries_sku = {value=var.registries_sku}
+    acrPushRolePrincipalId = {value=data.azurerm_client_config.current.object_id}
+    omsagent = {value=var.omsagent}
+    retentionInDays = {value=var.retentionInDays}
+    networkPolicy = {value=var.networkPolicy}
+    azurepolicy = {value=var.azurepolicy}
+    networkPlugin = {value=var.networkPlugin}
+    authorizedIPRanges = {value=var.authorizedIPRanges}
+    ingressApplicationGateway = {value=var.ingressApplicationGateway}
+    appGWcount = {value=var.appGWcount}
+    appGWsku = {value=var.appGWsku}
+    appGWmaxCount = {value=var.appGWmaxCount}
+    appgwKVIntegration = {value=var.appgwKVIntegration}
+    keyVaultAksCSI = {value=var.keyVaultAksCSI}
+    keyVaultCreate = {value=var.keyVaultCreate}
+    keyVaultOfficerRolePrincipalId = {value=data.azurerm_client_config.current.object_id}
+  })

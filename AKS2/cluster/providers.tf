d33af2ce93267fc316d11dd5701fd2d055a11318
Shailender Singh <50791698+shailenderbakshi@users.noreply.github.com>
2023-05-10 17:35:05 +0100

    added providers

--- AKS2/cluster/providers.tf
+++ AKS2/cluster/providers.tf
@@ -12,10 +12,10 @@ terraform {
  }
 
     backend "azurerm" {
-        resource_group_name  = var.rgname
-        storage_account_name = var.storage_account_name
-        container_name       = var.container_name
-        key                  = "terraform.tfstate"
+        resource_group_name  = "AZLZ-project-tstate-rg"
+        storage_account_name = "azlzprojecttfstate22513"
+        container_name       = "landingzonetfstate"
+        key                  = "aks-actions.tfstate"
     }
 }
 
@@ -23,6 +23,11 @@ provider "azurerm" {
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
+    }
+
+    key_vault {
+     purge_soft_delete_on_destroy    = true
+     recover_soft_deleted_key_vaults = true
     }
   }
 }
